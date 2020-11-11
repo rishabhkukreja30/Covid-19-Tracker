a88555803e5cff71a220ca2e2ad76ce5d0dd92cd
rishabh kukreja <rishabhkukreja30@gmail.com>
2020-11-11 23:34:01 +0530

    created infobox component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css'
 import React from 'react'
 import AppHeader from './components/AppHeader'
-
+import InfoBox from './components/InfoBox'
 
 function App() {
-
 
   return (
     <div className="App">
       <AppHeader />
+      <div className='app-stats'>
+      <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
+      <InfoBox title="Recovered" cases={123} total={2000} />
+      <InfoBox title="Deaths" cases={123} total={2000} />
+      </div>
     </div>
   );
 }
--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Card} from 'react-bootstrap'
 
-const InfoBox = () => {
+const InfoBox = ({title , cases, total}) => {
     return (
         <div>
-            
+            <Card style={{backgroundColor:'#e8e8e8'}} >
+            <Card.Body style={{padding:'10px'}}>
+                <Card.Title>
+                    <strong>{title}</strong>
+                </Card.Title>
+
+            <Card.Text>
+                <strong>{cases}</strong>
+            </Card.Text>
+
+            <Card.Text>
+                <strong>{total} Total</strong>
+            </Card.Text>
+
+            </Card.Body>
+            </Card>
+
         </div>
     )
 }
